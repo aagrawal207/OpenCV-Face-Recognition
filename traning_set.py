f3aa33ba571b3809cad923474d4a80398c5635aa
Abhishek-dev2 <aagrawal207@gmail.com>
2017-03-07 23:41:12 +0530

    Added comments to understand the code

--- traning_set.py
+++ traning_set.py
@@ -1,17 +1,19 @@
-import os
+import os                                                                       # import for taking the imagePaths
 import cv2
 import numpy as np
 from PIL import Image
 
-recognizer = cv2.createLBPHFaceRecognizer()
+recognizer = cv2.createLBPHFaceRecognizer()                                     #Local Binary Patterns Histograms
+# This is the common interface to train all of the available cv::FaceRecognizer implementations
 path = './dataset'                                                              # Folder where faces are saved
 
 def getImagesWithID(path):
-    imagePaths = [os.path.join(path, f) for f in os.listdir(path)]
-    faces = []
-    Ids = []
+    imagePaths = [os.path.join(path, f) for f in os.listdir(path)]              # Joining './dataset' and '<image names>'
+    faces = []                                                                  # Empty array for faces
+    Ids = []                                                                    # Empty array for Person Ids
     for imagePath in imagePaths:
-        faceImg = Image.open(imagePath).convert('L')                            # find what is this doing
+        # Converting colored and GrayScale images into bilevel images using Floyd-Steinberg dither
+        faceImg = Image.open(imagePath).convert('L')
         faceNp = np.array(faceImg, 'uint8')                                     # Converting face array into numpy array
         ID = int(os.path.split(imagePath)[-1].split('.')[1])                    # Check this again
         faces.append(faceNp)
@@ -22,5 +24,5 @@ def getImagesWithID(path):
 
 Ids, faces = getImagesWithID(path)                                              # Calling the function
 recognizer.train(faces, np.array(Ids))                                          # Training the faces
-recognizer.save('recognizer/trainingData.yml')                                  # Saving the yml file
+recognizer.save('./recognizer/trainingData.yml')                                # Saving the yml file
 cv2.destroyAllWindows()                                                         # Closing all the opened windows
