0d6b7adb6c8510ba0ec2245a824ed7105e10a42c
Abhishek-dev2 <aagrawal207@gmail.com>
2017-03-10 02:52:49 +0530

    Little updates everywhere

--- detect3.py
+++ detect3.py
@@ -1,18 +1,17 @@
 import cv2
-import numpy as np
+# import numpy as np
 
-detector= cv2.CascadeClassifier('haarcascade_frontalface_alt_tree.xml')
+detector= cv2.CascadeClassifier('./HaarCascade/haarcascade_frontalface_alt_tree.xml')
+
 cap = cv2.VideoCapture(0)
-
 while(True):
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = detector.detectMultiScale(gray, 1.3, 5)
-    if(len(faces)!=0):
-        for (x,y,w,h) in faces:
-            cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 255), 2)
 
-    cv2.imshow('frame',img)
+    cv2.imshow('img',img)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
