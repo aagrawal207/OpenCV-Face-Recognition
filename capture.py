b9237ecf70addfd7493a471805dd179c156e79d6
Abhishek-dev2 <aagrawal207@gmail.com>
2017-03-12 20:47:29 +0530

    recognize2.py is half way ready

---
+++ capture.py
@@ -0,0 +1,12 @@
+import cv2
+from matplotlib import pyplot as plt
+
+cam = cv2.VideoCapture(0)
+s, img = cam.read()
+
+
+plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
+plt.xticks([]), plt.yticks([]) # to hide tick values on X and Y
+plt.show()
+
+cv2.imwrite("test.jpg", img)
--- recognize2.py
+++ recognize2.py
@@ -1,53 +1,34 @@
-import cv2                                                                      # openCV
-import numpy as np                                                              # for numpy arrays
-import sqlite3
+import cv2
 import dlib
-from skimage import io
+import os
+import time
 
+cam = cv2.VideoCapture(0)
 detector = dlib.get_frontal_face_detector()
-# cap = cv2.VideoCapture('test_video.mp4')
-cap = cv2.VideoCapture(0)                                                       # defining which camera to take input from
+date = time.strftime("%d.%m.%Y")
+path = './pics_taken/' + date
+if not os.path.exists(path):
+    os.makedirs(path)
 
-def getProfile(id):
-    connect = sqlite3.connect("Face-DataBase")
-    cmd = "SELECT * FROM Students WHERE ID=" + str(id)
-    cursor = connect.execute(cmd)
-    profile = None
-    for row in cursor:
-        profile = row
-    connect.close()
-    return profile
+secs = 30
 
-rec = cv2.createLBPHFaceRecognizer()                                            # Local Binary Patterns Histograms
-rec.load('./recognizer/trainingData.yml')                                       # loading the trained data
-font = cv2.cv.InitFont(cv2.cv.CV_FONT_HERSHEY_PLAIN, 2, 1, 0, 1)                # the font of text on face recognition
+picNum = 1
 while(True):
-    ret, img = cap.read()                                                       # reading the camera input
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)                                # conveting the camera input into GrayScale
+    ret, img = cam.read()
     dets = detector(img, 1)
+    folderName = path + '/pic' + str(picNum)
+    if not os.path.exists(folderName):
+        os.makedirs(folderName)
     for i, d in enumerate(dets):
+        picName = str(i + 1) + '.jpg'
+        picFolderName = folderName + '/' + picName
         cv2.rectangle(img, (d.left(), d.top()), (d.right(), d.bottom()), (255, 255, 255), 2)
-        id, conf = rec.predict(gray[d.top():d.bottom(), d.left():d.right()])                          # Comparing from the trained data
-        if conf < 100:
-            profile = getProfile(id)
-            if profile != None:
-                cv2.cv.PutText(cv2.cv.fromarray(img),
-                                profile[1] + str("(%.2f)" % conf),
-                                (d.left(), d.bottom()),
-                                font,
-                                (0, 0, 0))                                # Writing the name of the face recognized
-        else :
-            cv2.cv.PutText(cv2.cv.fromarray(img),
-                            "Unknown" + str(conf),
-                            (d.left(), d.bottom()),
-                            font,
-                            255)                                            # Writing the name of the face recognized
-
-
-    cv2.imshow('frame',img)                                                     # Showing each frame on the window
-    k = cv2.waitKey(30) & 0xff                                                  # Turn off the recognizer using Esc Key
+        cv2.imwrite(picFolderName, img[d.top():d.bottom(), d.left():d.right()])
+    picNum += 1
+    # cv2.imshow('frame',img)                                                   # Showing each frame on the window
+    k = cv2.waitKey(5000) & 0xff                                                # Turn off the recognizer using Esc Key
     if k == 27:
         break
 
-cap.release()                                                                   # turning the webcam off
+cam.release()                                                                   # turning the webcam off
 cv2.destroyAllWindows()                                                         # Closing all the opened windows
