9ebda2f022808737f2a5cf6baf1600ea580954b7
Abhishek-dev2 <aagrawal207@gmail.com>
2017-03-15 01:05:56 +0530

    Now onli pic is saved and not its folder

--- dataset_creator2.py
+++ dataset_creator2.py
@@ -3,12 +3,10 @@ import numpy as np                                                              
 import sqlite3
 import dlib
 import os                                                                       # for creating folders
-import openface                                                                 # for aligning the faces
 
 # cap = cv2.VideoCapture('video_for_training.mp4')
 cap = cv2.VideoCapture(0)
 detector = dlib.get_frontal_face_detector()
-align = openface.AlignDlib('./shape_predictor_68_face_landmarks.dat')
 
 def insertOrUpdate(Id, Name, roll) :                                            # this function is for database
     connect = sqlite3.connect("Face-DataBase")                                  # connecting to the database
@@ -44,14 +42,8 @@ while(True):
     dets = detector(img, 1)
     for i, d in enumerate(dets):                                                # loop will run for each face detected
         sampleNum += 1
-        imgDetected = img[d.top():d.bottom(), d.left():d.right()]
-        imgaligned = align(imgDim=96,
-                                    rgbImg=imgDetected,
-                                    bb=None,
-                                    landmarks=None,
-                                    landmarkIndices=INNER_EYES_AND_BOTTOM_LIP)
         cv2.imwrite(folderPath + "/User." + Id + "." + str(sampleNum) + ".jpg",
-                    imgaligned)                                                # Saving the faces
+                    img[d.top():d.bottom(), d.left():d.right()])                                                # Saving the faces
         cv2.rectangle(img, (d.left(), d.top())  ,(d.right(), d.bottom()),(0,255,0) ,2) # Forming the rectangle
         cv2.waitKey(200)                                                        # waiting time of 200 milisecond
     cv2.imshow('frame', img)                                                    # showing the video input from camera on window
--- recognize2.py
+++ recognize2.py
@@ -32,15 +32,18 @@ while(True):
     ret, img = cam.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)                                # conveting the camera input into GrayScale
     dets = detector(img, 1)
-    folderName = path + '/pic' + str(picNum)
-    if not os.path.exists(folderName):
-        os.makedirs(folderName)
+    # folderName = path + '/pic' + str(picNum)
+    # if not os.path.exists(folderName):
+    #     os.makedirs(folderName)
+    totalConf = 0.0
+    faceRec = 0
     for i, d in enumerate(dets):
-        picName = str(i + 1) + '.jpg'
-        picFolderName = folderName + '/' + picName
-        # Add alignment of faces before recognitioin
+        # picName = str(i + 1) + '.jpg'
+        # picFolderName = folderName + '/' + picName
         id, conf = rec.predict(gray[d.top():d.bottom(), d.left():d.right()])    # Comparing from the trained data
         if conf < 150:
+            totalConf += conf
+            faceRec += 1
             profile = getProfile(id)
             if profile != None:
                 cv2.cv.PutText(cv2.cv.fromarray(img),
@@ -54,11 +57,16 @@ while(True):
                             (d.left(), d.bottom()),
                             font,
                             255)                                                # Writing the name of the face recognized
-        # keep a single picture with squares instead of multiple faces in different folder
-        cv2.imwrite(picFolderName, img[d.top():d.bottom(), d.left():d.right()])
+        # cv2.imwrite(picFolderName, img[d.top():d.bottom(), d.left():d.right()])
         cv2.rectangle(img, (d.left(), d.top()), (d.right(), d.bottom()), (255, 255, 255), 2)
+    cv2.imshow('frame', img)                                                     # Showing each frame on the window
+    cv2.imwrite(path + '/pic' + str(picNum) + '.jpg', img)
+    # detectPrint = 'Frame' + str(picNum) + ". %d face detected" % len(dets)
+    # if faceRec != 0:
+    #     print detectPrint + " and ", faceRec, " face recognized with confidence %.2f"%(totalConf / faceRec)
+    # else:
+    #     print detectPrint + " and 0 faces recognized"
     picNum += 1
-    cv2.imshow('frame',img)                                                     # Showing each frame on the window
     k = cv2.waitKey(10) & 0xff                                                  # Turn off the recognizer using Esc Key
     if k == 27:
         break
