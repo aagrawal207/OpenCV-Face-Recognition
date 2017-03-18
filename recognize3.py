e47fc1c2ee47581b5af9488a11e9cb39303cbaf5
Abhishek-dev2 <aagrawal207@gmail.com>
2017-03-18 19:33:22 +0530

    Added recognize3.py

---
+++ recognize3.py
@@ -0,0 +1,17 @@
+import cv2
+import dlib
+import os
+import time
+import sqlite3
+
+cam = cv2.VideoCapture(1)
+detector = dlib.get_frontal_face_detector()
+
+picNum = 1
+img = cv2.imread('test1.jpg')
+dets = detector(img, 1)
+for i, d in enumerate(dets):
+    cv2.imwrite('./Croped_faces/face' + str(picNum) + '.jpg', img[d.top():d.bottom(), d.left():d.right()])
+    picNum += 1
+
+cv2.destroyAllWindows()                                                         # Closing all the opened windows
